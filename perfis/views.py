50439704277afe4bf06b988949fc7f52789f8867
RomeiroMaria <maria.alves246@gmaill.com>
2021-10-22 16:04:39 -0300

    3- criação página Perfil

--- perfis/views.py
+++ perfis/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+    perfil = Perfil()
+
+    if perfil_id == 1:
+        perfil = Perfil('Maria Alves', 'maria.alves@ifb.edu.br', '112233445', 'IFB')
+    
+    if perfil_id == 2:
+        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
+    
+    return render(request, 'perfil.html', {'perfil' : perfil})
