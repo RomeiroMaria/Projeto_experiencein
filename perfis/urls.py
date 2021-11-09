43a75e9e1df204cb732f269f0a8a0f8008d8c5d9
RomeiroMaria <maria.alves246@gmaill.com>
2021-11-09 16:11:53 -0300

    Selecionando perfis de uma lista

--- perfis/urls.py
+++ perfis/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # corresponde ao caminho da nossa âncora na página index.html
     path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
 ]
