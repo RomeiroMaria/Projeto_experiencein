2ec7cebad417f9690c7cf9915c91c3beaab0df42
RomeiroMaria <maria.alves246@gmaill.com>
2022-01-03 17:11:51 -0300

    12 - Atualização

--- perfis/models.py
+++ perfis/models.py
@@ -5,23 +5,25 @@ from django.db.models.deletion import CASCADE
 # Create your models here.
 
 class Perfil(models.Model):
-    nome = models.CharField(max_length=255, null=False)
-    
-    telefone = models.CharField(max_length=15, null=False)
-    nome_empresa = models.CharField(max_length=255, null=False)
 
-    contatos = models.ManyToManyField('self')
+	nome = models.CharField(max_length=255, null=False)
+	telefone = models.CharField(max_length=15, null=False)
+	nome_empresa = models.CharField(max_length=255, null=False)
+	contatos = models.ManyToManyField('self')
+	usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
 
-    usuario = models.OneToOneField(User, on_delete= CASCADE, related_name="perfil")
-    @property # apagar o atributo email e delegar seu acesso para a classe User
-    def convidar(self, perfil_convidado):
-        Convite(solicitante=self, convidado=perfil_convidado).save()
+	@property
+	def email(self):
+		return self.usuario.email
+
+	def convidar(self, perfil_convidado):
+		Convite(solicitante=self, convidado=perfil_convidado).save()
 
 class Convite(models.Model):
-    solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_feitos')
-    convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_recebidos')
+	solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_feitos')
+	convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_recebidos')
 
-    def aceitar(self):
-        self.convidado.contatos.add(self.solicitante)
-        self.solicitante.contatos.add(self.convidado)
-        self.delete()
+	def aceitar(self):
+		self.convidado.contatos.add(self.solicitante)
+		self.solicitante.contatos.add(self.convidado)
+		self.delete()
--- perfis/views.py
+++ perfis/views.py
@@ -5,16 +5,16 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-	return render(request, 'index.html', {'perfis': Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
+    return render(request, 'index.html', {'perfis':Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
 
 @login_required
-def exibir(request, perfil_id):	
-	perfil = Perfil.objects.get(id=perfil_id)
+def exibir(request, perfil_id):
 
-	perfil_logado = get_perfil_logado(request)
-	ja_e_contato = perfil in perfil_logado.contatos.all()
-	
-	return render(request, 'perfil.html', {'perfil':perfil, "ja_e_contato":ja_e_contato})
+    perfil = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    ja_e_contato = perfil in perfil_logado.contatos.all()
+
+    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 'ja_e_contato' : ja_e_contato})
 
 @login_required
 def convidar(request, perfil_id):
@@ -25,9 +25,9 @@ def convidar(request, perfil_id):
 
 @login_required
 def aceitar(request, convite_id):
-	convite = Convite.objects.get(id=convite_id)
-	convite.aceitar()
-	return redirect('index')
+    convite = Convite.objects.get(id=convite_id)
+    convite.aceitar()
+    return redirect('index')
 
 @login_required
 def get_perfil_logado(request):
